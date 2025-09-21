46cba0c9a57ef6b3f273ad10d2b0c0a7d9be5433
Tim Raymond <tcraymond@inductivereasoning.com>
2025-09-21 19:49:58 -0400

    Add FEM material definitions and regions to MainModel constructor for enhanced simulation capabilities

--- MTLTestUI/MainModel.cs
+++ MTLTestUI/MainModel.cs
@@ -14,6 +14,7 @@ using MathNet.Numerics.LinearAlgebra;
 using Vector_d = MathNet.Numerics.LinearAlgebra.Vector<double>;
 using MeshLib;
 using TfmrLib;
+using TfmrLib.FEM;
 using GeometryLib;
 
 namespace MTLTestUI
@@ -53,7 +54,79 @@ namespace MTLTestUI
             geometry = Measure("GenerateGeometry", () => tfmr.GenerateGeometry());
             var meshgen = Measure("MeshGenerator ctor", () => new MeshGenerator());
             Measure("AddGeometry", () => meshgen.AddGeometry(geometry));
-            //mesh = Measure("GenerateMesh", () => meshgen.GenerateMesh(100.0, 1));
+            mesh = Measure("GenerateMesh", () => meshgen.GenerateMesh("bin/Debug/net9.0/case.geo",1000.0, 1));
+
+            double freq = 60.0;
+            int excitedTurn = 0;
+            int excitedStrand = 0;
+            int order = 1;
+
+            Console.WriteLine($"Frequency: {freq.ToString("0.##E0")} Turn: {excitedTurn}");
+
+            var fem = new GetDPAxiMagProblem();
+
+            var oil = new Material("Oil")
+            {
+                Properties = new Dictionary<string, double> {
+                { "mu_r", 1.0 },
+                { "epsr", tfmr.eps_oil },
+                { "loss_tan", tfmr.ins_loss_factor } }
+            };
+
+            var paper = new Material("Paper")
+            {
+                Properties = new Dictionary<string, double> {
+                { "mu_r", 1.0 },
+                { "epsr", tfmr.Windings[0].eps_paper },
+                { "loss_tan", tfmr.ins_loss_factor } }
+            };
+
+            var copper = new Material("Copper")
+            {
+                Properties = new Dictionary<string, double> {
+                { "mu_r", 1.0 },
+                { "sigma", 5.96e7 } }
+            };
+
+            fem.Materials.Add(oil);
+            fem.Materials.Add(paper);
+            fem.Materials.Add(copper);
+            fem.Regions.Add(new Region() { Name = "InteriorDomain", Tags = new List<int>() { tfmr.TagManager.GetTagByString("InteriorDomain") }, Material = oil });
+            fem.BoundaryConditions.Add(new BoundaryCondition() { Name = "Dirichlet", Tags = new List<int>() { tfmr.TagManager.GetTagByString("CoreLeg"), tfmr.TagManager.GetTagByString("TopYoke"), tfmr.TagManager.GetTagByString("BottomYoke"), tfmr.TagManager.GetTagByString("RightEdge") } });
+            int globalTurn = -1;
+            for (int wdgNum = 0; wdgNum < tfmr.Windings.Count; wdgNum++)
+            {
+                var wdg = tfmr.Windings[wdgNum];
+                for (int segNum = 0; segNum < wdg.Segments.Count; segNum++)
+                {
+                    var seg = wdg.Segments[segNum];
+                    if (seg.Geometry != null)
+                    {
+                        var seg_geom = seg.Geometry;
+                        for (int localTurn = 0; localTurn < seg_geom.NumTurns; localTurn++, globalTurn++)
+                        {
+                            for (int localStrand = 0; localStrand < seg_geom.NumParallelConductors; localStrand++)
+                            {
+                                var locKey = new LocationKey(wdgNum, segNum, localTurn, localStrand);
+                                var regionIns = new Region() { Name = $"Wdg{wdgNum}Turn{localTurn}Std{localStrand}Ins", Tags = new List<int>() { tfmr.TagManager.GetTagByLocation(locKey, TagType.InsulationSurface) }, Material = paper };
+                                var regionCond = new Region() { Name = $"Wdg{wdgNum}Turn{localTurn}Std{localStrand}Cond", Tags = new List<int>() { tfmr.TagManager.GetTagByLocation(locKey, TagType.ConductorSurface) }, Material = copper };
+                                fem.Regions.Add(regionIns);
+                                fem.Regions.Add(regionCond);
+                                if (globalTurn == excitedTurn && localStrand == excitedStrand)
+                                {
+                                    fem.Excitations.Add(new Excitation() { Region = regionCond, Value = 1.0 });
+                                }
+                                else
+                                {
+                                    fem.Excitations.Add(new Excitation() { Region = regionCond, Value = 0.0 });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            fem.Solve();
 
             total.Stop();
             Console.WriteLine($"MainModel constructor total: {total.Elapsed.TotalMilliseconds:F3} ms");
